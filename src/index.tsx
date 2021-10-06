9ce69fbb426876eae63a934fd85eaf8757731162
Italo Sousa <italo@italosousa.com.br>
2021-10-05 21:30:05 -0300

    Created repository for F1 App

---
+++ src/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+
+export default function Main() {
+  return (
+    <>
+      <StatusBar />
+      <View>
+        <Text>Afonso viado</Text>
+      </View>
+    </>
+  );
+}
---
+++ src/theme/MainTheme/colors.ts
@@ -0,0 +1,9 @@
+import React from 'react';
+
+interface Colors {
+  color: String;
+}
+const colors = {
+  primary: '#E10600',
+  secondary: '#1F1F27',
+};
